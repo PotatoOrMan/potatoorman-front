7a221436e3a6c5b50695ee1bd659b95093cf3449
wlsooo <kwonjisoo14@gmail.com>
2024-04-18 10:28:56 +0900

    images 업로드

---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+import "./css/reset.css";
+import "./css/home.css";
+
+export default function Home() {
+    return <div className="background">
+        <img src="../images/logo.png"/>
+    </div>
+}
