82434f27486374ef02c82f72aa1c6cf5eead19fb
wlsooo <kwonjisoo14@gmail.com>
2024-04-19 15:10:39 +0900

    home, story

--- src/components/story.js
+++ src/components/story.js
@@ -1,22 +1,49 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import "./css/story.css";
 
-function InfoBox() {
-    return <div className="info-box">
-        <img src="../images/ect/info_box.png" alt="infoBox" className="infoImg"/>
-    </div>
+function InfoBox({ text, changeIndex }) {
+    return (
+        <>
+            <div className="person-text">나</div>
+            <div className="text-box">{text}</div>
+            <i className="bi bi-caret-right-fill" onClick={() => {
+                    changeIndex();
+            }}></i>
+        </>
+    );
 }
 
 export default function Story() {
-    const [bgImg, setBgImg] = useState("../images/backgrounds/story_background1.png");
-    
+    const storyData = {
+        text: ["아 역시 할 것도 없고 심심하네", "어? 저게 할머니 감자밭인가?", "음? 뭐지?"],
+        bgImg: [
+            "../images/backgrounds/story_background1.png",
+            "../images/backgrounds/story_background2.png",
+            "../images/backgrounds/story_background3.png"
+        ]
+    };    
+    const [index, setIndex] = useState(0);
+    const navigate = useNavigate(); 
+
+    const changeIndex = () => {
+        if (index !== 2) {
+            setIndex(index + 1);
+        } else {
+            navigate('/')
+        }
+    };
+
+    const bgImg = storyData.bgImg[index];
+    const text = storyData.text[index];
+
     useEffect(() => {
         document.body.style.backgroundImage = `url(${bgImg})`;
     }, [bgImg]);
 
     return (
         <div className="storyDiv">
-            <InfoBox/>
+            <InfoBox text={text} changeIndex={changeIndex}/>
         </div>
-    )
+    );
 }
