9cfa571ca12236b82484d607efaff6efc629818c
wlsooo <kwonjisoo14@gmail.com>
2024-05-14 16:06:47 +0900

    할일 정리

--- src/components/roundmanager.js
+++ src/components/roundmanager.js
@@ -6,6 +6,8 @@ export default function RoundManager() {
     const index = location.state.index; //stroy에서 넘긴 index값 저장
 
     // 이미지, 감자인덱스 매개변수로 넘기기, 필요없을 수도 있는 컴포넌트, round에서 관리할 수 있는지 생각해보기
+    // 감자 인덱스와 라운드 인덱스 관리
+    // setInterval로 10초마다 다른 라운드 호출, 라운드 인덱스 증가 
     switch(index) {
         case 1: return <Round />
         case 2: return <Round />
