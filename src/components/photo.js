3d3d2a90c5b7fd6a0472beca3741c426193f47d0
wlsooo <kwonjisoo14@gmail.com>
2024-05-31 10:28:04 +0900

    웹캠 연결

--- src/components/photo.js
+++ src/components/photo.js
@@ -1,16 +1,69 @@
-import { useEffect, useState } from "react"
-import './css/photo.css'
+import React, { useEffect, useState } from "react";
+import './css/photo.css';
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user",
+};
+
+const WebcamCapture = ({ onCapture }) => (
+  <Webcam
+    audio={false}
+    height={720}
+    screenshotFormat="image/png"
+    width={1280}
+    videoConstraints={videoConstraints}
+  >
+    {({ getScreenshot }) => (
+      <button
+        onClick={() => {
+          const imageSrc = getScreenshot();
+          onCapture(imageSrc);
+        }}
+      >
+        사진 찍기
+      </button>
+    )}
+  </Webcam>
+);
 
 export default function Photo() {
-    const [bgImg, setBgImg] = useState('../images/backgrounds/play_background1.png')
+    const [bgImg, setBgImg] = useState('url(../images/backgrounds/play_background1.png)')
+    const [showModal, setShowModal] = useState(true)
+    const [capturedImage, setCapturedImage] = useState(null);
+
     useEffect(() => {
-        document.body.style.backgroundImage = `url(${bgImg}})`;
+        document.body.style.backgroundImage = bgImg;
     }, [bgImg])
 
+    // spcae 누르면 모달창 사라지고 사진 찍을 수 있도록 설정
+    useEffect(() => {
+        const handleModal = (e) => {
+            if(e.key === 'Space' || e.key === ' ') setShowModal(false)
+        }
+        document.addEventListener('keydown', handleModal)
+        return () => document.removeEventListener('keydown', handleModal)
+    }, [showModal])
+
     return (
-        <div className='roundmodalContainer'>
-            <text className='modalText'>사진을 찍어주세요!</text>
+        showModal ? 
+        <div className="photoModalContainer">
+          <div className='photoModal'>
+            <p className='modalText'>사진을 찍어주세요!</p>
             <div className='spacebar'>space</div>
+          </div>
         </div>
-    )
+        :
+        <>
+          {capturedImage ? (
+            <div className="capturedImageContainer">
+              <img src={capturedImage} alt="Captured" />
+            </div>
+          ) : (
+            <WebcamCapture onCapture={setCapturedImage} />
+          )}
+        </>
+    );
 }
