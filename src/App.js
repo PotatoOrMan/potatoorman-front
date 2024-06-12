0d594f779af5062b24a6adcdea7f167b6f6eb83d
JISOO <kwonjisoo14@gmail.com>
2024-06-12 16:00:53 +0900

    배경음악 넣기

--- src/App.js
+++ src/App.js
@@ -8,23 +8,25 @@ import Round from "./components/round"
 import Storymanager from './components/storymanager';
 import ResultPlayview2 from './components/resultPlayview2';
 import Photo from './components/photo'
+import BackgroundMusic from './backgroundMusic'
 
 function App() {
   return (
     <div className='App'>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />}></Route>
-					<Route path="/gallery" element={<Gallery />}></Route>
-					<Route path="/story" element={<Story />}></Route>
-					<Route path="/choice" element={<Choice />}></Route>
-					<Route path="/round" element={<Round />}></Route>
-					<Route path="/storymanager" element={<Storymanager />}></Route>
-					<Route path="/resultPlayview2" element={<ResultPlayview2 />}></Route>
-					<Route path="/photo" element={<Photo />}></Route>
-				</Routes>
-			</BrowserRouter>
-		</div>
+		<BackgroundMusic />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/gallery" element={<Gallery />}></Route>
+				<Route path="/story" element={<Story />}></Route>
+				<Route path="/choice" element={<Choice />}></Route>
+				<Route path="/round" element={<Round />}></Route>
+				<Route path="/storymanager" element={<Storymanager />}></Route>
+				<Route path="/resultPlayview2" element={<ResultPlayview2 />}></Route>
+				<Route path="/photo" element={<Photo />}></Route>
+			</Routes>
+		</BrowserRouter>
+	</div>
   );
 }
 
